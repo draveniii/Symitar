b667fcdc0b7a8693683795da54dfd0bba45289ad
draven-mcconathy <draveniii@gmail.com>
2024-06-04 15:20:34 -0400

    Initial commit for MemberClass, contains member data, setters, and getters

---
+++ MemberClass.py
@@ -0,0 +1,25 @@
+#Member Class
+#Draven McConathy
+#05/31/2024
+#Contains information about the member in question including name, address, and date of birth
+
+class Member:
+    def __init__(self, name, address, dateOfBirth):
+        self.memberName = name
+        self.memberAddress = address
+        self.memberDOB = dateOfBirth
+
+    def change_member_name(self, newMemberName):
+        self.memberName = newMemberName
+
+    def change_member_address(self, newMemberAddress):
+        self.memberAddress = newMemberAddress
+
+    def get_memberName(self):
+        return self.memberName 
+
+    def get_memberAddress(self):
+        return self.memberAddress
+
+    def get_memberDOB(self):
+        return self.memberDOB
