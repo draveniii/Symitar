699787477e51cebd7d3f764ada53fe1b641d9285
draven-mcconathy <draveniii@gmail.com>
2024-06-04 19:57:30 -0400

    Added a menu and menu inputs on main

--- main.py
+++ main.py
@@ -3,14 +3,35 @@
 #Imitation of Symitar, a core prossesing system for financial institutions
 
 def main():
-    print("Welcome to Symitar\n")
+    print("Welcome to Symitar\n\n")
 
-    currentAccount = get_account_number()
-    print(currentAccount)
+    userInput = None
+
+    #Shows menu and gets user input
+    while userInput != 3:
+        print_menu()
+        userInput = get_menuInput()
+        if userInput == 1:
+            get_account_number
+
+def print_menu():
+    print("1. Find an account\n")
+    print("2. Create an Account\n")
+    print("3. Quit\n")
 
 def get_account_number():
-    inputAccountNumber = input("Please enter a four digit account number: ") #Gets account number from user
+    inputAccountNumber = input("Please enter an account number: ") #Gets account number from user
     return inputAccountNumber
+
+def get_menuInput():
+    userInput = input()
+
+    #Makes sure userInput is valid and continues to get input if it is not
+    while userInput < 1 or userInput > 3:
+        userInput = input("Please enter one of the menu options 1-3: ")
+    
+    return userInput
+        
 
 if __name__ == "__main__":
     main()
