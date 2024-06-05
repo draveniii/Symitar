6f2b5df41361387d3e6681a8b8b59bdb3971ed64
draven-mcconathy <draveniii@gmail.com>
2024-06-05 15:13:59 -0400

    Updated with methods to add shares and members to an account

--- BankAccountClass.py
+++ BankAccountClass.py
@@ -3,13 +3,21 @@
 #Bank account class that keeps and manages account holder information and account funds
 
 class BankAccount:
-    def __init__(self, newAccountNumber, newMember):
-        
+    def __init__(self, newAccountNumber):
         self.accountNumber = newAccountNumber
-        self.member = newMember
-        self.savings
+        self.members = []
+        self.shares = []
 
+    def get_accountNumber(self):
+        return self.accountNumber
 
+    def add_member(self, newMember):
+        self.members.append(newMember)
+
+    def add_share(self, newShare):
+        self.shares.append(newShare)
+
+    
         
                  
 
