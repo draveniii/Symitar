080531c8f2639a74543f082c11b1a756490c3a0d
draven-mcconathy <draveniii@gmail.com>
2024-06-07 16:07:15 -0400

    Actually removed initialDeposit from the constructor of the share class

--- ShareClass.py
+++ ShareClass.py
@@ -4,12 +4,12 @@
 #Outlines the contents on a share including share name, share ID, interest rate, and minimum balance
 
 class Share:
-    def __init__(self, IDNumber, name, rate, minimumBalance, initialDeposit):
+    def __init__(self, IDNumber, name, rate, minimumBalance):
         self.shareID = IDNumber
         self.shareName = name
         self.interestRate = rate
         self.minimumBalance = minimumBalance
-        self.currentBalance = initialDeposit
+        self.currentBalance = 0
  
     def change_interestRate(self, newInterestRate):
         self.interestRate = newInterestRate
