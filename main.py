c1338f19d4cdedfb1a55d9c6fd221e89ed317e2d
draven-mcconathy <draveniii@gmail.com>
2024-06-07 17:34:28 -0400

    Added the ability to create an account and add members and shares to said account

--- main.py
+++ main.py
@@ -2,8 +2,14 @@
 #05/29/2024
 #Imitation of Symitar, a core prossesing system for financial institutions
 
+from BankAccountClass import *
+
 def main():
+
     print("Welcome to Symitar\n\n")
+    
+    #array that stores created accounts
+    accountsArray = []
 
     userInput = None
 
@@ -12,26 +18,47 @@ def main():
         print_menu()
         userInput = get_menuInput()
         if userInput == 1:
-            get_account_number
+            pass
+        elif userInput == 2:
+            create_account(accountsArray)
+        else:
+            print("Goodbye!")
 
 def print_menu():
     print("1. Find an account\n")
     print("2. Create an Account\n")
     print("3. Quit\n")
 
+#Gets account number from user and returns that number
 def get_account_number():
-    inputAccountNumber = input("Please enter an account number: ") #Gets account number from user
+    inputAccountNumber = int(input("Please enter an account number: "))
     return inputAccountNumber
 
+#Gets an input from user for menu selection and validates said input
 def get_menuInput():
-    userInput = input()
+    userInput = int(input())
 
     #Makes sure userInput is valid and continues to get input if it is not
     while userInput < 1 or userInput > 3:
-        userInput = input("Please enter one of the menu options 1-3: ")
+        userInput = int(input("Please enter one of the menu options 1-3: "))
     
     return userInput
-        
+
+#Generates an account number, collects information needed to create account, and creates a BankAccount object which is stored in the "accounts" array
+def create_account(accountsArray):
+   #Creates the new account
+   newAccount = BankAccount()
+
+   #Creates a member object and adds the member to the account
+   newAccount.add_member()
+
+   #Creates a share and adds the share to the account
+   newAccount.add_share()
+
+   #adds created account to the accountsArray
+   accountsArray.append(newAccount)
+
+
 
 if __name__ == "__main__":
     main()
