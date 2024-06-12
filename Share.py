023d2a286542c058dfd3e4b1dda47b1230cd8073
draven-mcconathy <draveniii@gmail.com>
2024-06-12 18:50:38 -0400

    Updated file name and updated methods for UI compatability

---
+++ Share.py
@@ -0,0 +1,28 @@
+#Share Class
+#Draven McConathy  
+#06/04/2024
+#Outlines the contents on a share including share name, share ID, interest rate, and minimum balance
+
+class Share:
+    def __init__(self, IDNumber, name, rate, minimumBalance):
+        self.shareID = IDNumber
+        self.shareName = name
+        self.interestRate = rate
+        self.minimumBalance = minimumBalance
+        self.currentBalance = 0
+ 
+    def change_interestRate(self, newInterestRate):
+        self.interestRate = newInterestRate
+
+    def withdraw_funds(self, amountToWithdraw):
+        #If the account does not have the funds to cover the withdraw, stop the withdraw.
+        if amountToWithdraw <= self.currentBalance - self.minimumBalance:
+            self.currentBalance -= amountToWithdraw
+        else:
+            print("Non sufficient funds")
+
+    def deposit_funds(self, amountToDeposit):
+        self.currentBalance += amountToDeposit
+
+    
+    
